a31e3fc7b9f2ea8e01485fbeffbb78e9e4357023
Sohrab momand <sohrabmomand.work@gmail.com>
2023-11-09 10:18:57 +0430

    making a count up and down buttons with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+
+  const [count, setCount] = useState(0);
+
+
+  const date = new Date("November 9 2023");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setCount(c => c - 1)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount(c => c + 1)}>-</button>
+      </div>
+
+      <p>
+        <span></span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+
+    </div>
+  );
+}
